be75537c11ba781309060f9d5aecea6884c1a463
kemalreza12 <59290848+kemalreza12@users.noreply.github.com>
2020-01-18 08:56:52 +0700

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,9 @@
+var huruf = ['A','R','K','A','D','E','M','Y'];
+    console.log(huruf[0]);
+    console.log(huruf[3]);
+    console.log(huruf[5]);
+    console.log(huruf[1]);
+    console.log(huruf[2]);
+    console.log(huruf[4]);
+    console.log(huruf[6]);
+    console.log(huruf[7]);
